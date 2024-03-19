f716042fcfa660319d0440a5fb5a8b5a0a1268cf
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-03-19 20:58:47 +0200

    ContactNo updated, factoryTest
    person contact number changed from long to String, adminFactory tested

--- src/main/java/za/ac/cput/Factory/PersonFactory.java
+++ src/main/java/za/ac/cput/Factory/PersonFactory.java
@@ -4,7 +4,7 @@ import za.ac.cput.Domain.Person;
 import za.ac.cput.Utils.PersonUtils;
 
 public class PersonFactory {
-    public static Person buildPerson(String name, String surname, String email, String password, Long contactNo){
+    public static Person buildPerson(String name, String surname, String email, String password, String contactNo){
         if(PersonUtils.isValid(name)
                 || PersonUtils.isValid(surname)
                 || PersonUtils.isValid(email)
--- src/main/java/za/ac/cput/Repository/IAdminRepository.java
+++ src/main/java/za/ac/cput/Repository/IAdminRepository.java
@@ -1,6 +1,9 @@
 package za.ac.cput.Repository;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import za.ac.cput.Domain.Admin;
 
 public interface IAdminRepository extends JpaRepository<Admin, String> {

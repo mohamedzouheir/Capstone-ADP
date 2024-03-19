f79a2d89fa42b3a00fdef7261da4de4aa3d5b309
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-03-19 15:30:52 +0200

    base project

---
+++ src/test/java/za/ac/cput/capstoneadp/CapstoneAdpApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.capstoneadp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneAdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

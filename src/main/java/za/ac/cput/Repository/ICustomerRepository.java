b5beaa4fc6360d5353bd07d77873ba54660a05f8
AaishahVW <aaishavanwitt@gmail.com>
2024-03-26 19:03:29 +0200

    Customer Interface & Repository
    Completed Customer Interface & Repository

--- src/main/java/za/ac/cput/Repository/ICustomerRepository.java
+++ src/main/java/za/ac/cput/Repository/ICustomerRepository.java
@@ -1,4 +1,27 @@
 package za.ac.cput.Repository;
 
-public interface ICustomerRepository {
+/*
+Customer.Java
+Customer Model Class
+Author: Aa'ishah Van Witt
+Date: 26 March 2024
+ */
+
+import za.ac.cput.Domain.Order;
+import za.ac.cput.Domain.Person;
+
+import java.util.List;
+
+public interface ICustomerRepository <T, Id>{
+
+    public T getUserByPerson(Person person);
+    public T getLoginDetails(Person person);
+    List<Order> getAllOrders(T obj);
+    public T create(T obj);
+    public T read(Id obj);
+    public T update(T obj);
+    public T delete(T obj);
+    public List<T> getAll();
+
+
 }

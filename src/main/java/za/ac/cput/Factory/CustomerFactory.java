bc5f6db83032131b52111daf047667e9c8ee5adf
AaishahVW <aaishavanwitt@gmail.com>
2024-03-26 17:11:12 +0200

    Customer Utils & Factory
    Customer Utils and Factory Complete

--- src/main/java/za/ac/cput/Factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/Factory/CustomerFactory.java
@@ -1,4 +1,41 @@
 package za.ac.cput.Factory;
 
+/*
+Customer.Java
+Customer Model Class
+Author: Aa'ishah Van Witt
+Date: 26 March 2024
+ */
+
+import jakarta.validation.Valid;
+import za.ac.cput.Domain.Customer;
+import za.ac.cput.Domain.Order;
+import za.ac.cput.Domain.Person;
+import za.ac.cput.Utils.CustomerUtils;
+
+import java.util.List;
+
 public class CustomerFactory {
+
+    public static Customer buildCustomer(@Valid Person person, @Valid String customerId, @Valid List<Order> orders) {
+        if (CustomerUtils.isValid(person) || CustomerUtils.isValid(customerId) || CustomerUtils.isValid(orders)) {
+            return new Customer.Builder()
+                    .setIdentity(person)
+                    .setCustomerId(customerId)
+                    .setOrders(orders)
+                    .build();
+        } else {
+            return null;
+        }
+    }
+
+    public static Customer buildCustomer(@Valid Person person) {
+        if (CustomerUtils.isValid(person)) {
+            return new Customer.Builder().setIdentity(person)
+                    .setCustomerId(CustomerUtils.generateId())
+                    .build();
+        } else {
+            return null;
+        }
+    }
 }
---
+++ src/main/java/za/ac/cput/Utils/CustomerUtils.java
@@ -0,0 +1,33 @@
+package za.ac.cput.Utils;
+
+/*
+Customer.Java
+Customer Model Class
+Author: Aa'ishah Van Witt
+Date: 26 March 2024
+ */
+
+import za.ac.cput.Domain.Order;
+import za.ac.cput.Domain.Person;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CustomerUtils {
+
+    public static Boolean isValid(Person obj) {
+        return obj != null;
+    }
+
+    public static Boolean isValid(String obj) {
+        return !obj.isEmpty() || !obj.isBlank();
+    }
+
+    public static Boolean isValid(List<Order> obj) {
+        return obj != null;
+    }
+
+    public static String generateId() {
+        return UUID.randomUUID().toString();
+    }
+}

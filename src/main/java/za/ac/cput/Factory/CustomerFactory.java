c5ed961ae150cf03820c96d3d1bc9dc328b8b4cd
AaishahVW <aaishavanwitt@gmail.com>
2024-03-27 11:14:08 +0200

    Update Customer Factory
    Update Customer Factory

--- src/main/java/za/ac/cput/Factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/Factory/CustomerFactory.java
@@ -1,8 +1,8 @@
 package za.ac.cput.Factory;
 
 /*
-Customer.Java
-Customer Model Class
+CustomerFactory.Java
+Customer Factory Class
 Author: Aa'ishah Van Witt
 Date: 26 March 2024
  */
@@ -17,12 +17,12 @@ import java.util.List;
 
 public class CustomerFactory {
 
-    public static Customer buildCustomer(@Valid Person person, @Valid String customerId, @Valid List<Order> orders) {
-        if (CustomerUtils.isValid(person) || CustomerUtils.isValid(customerId) || CustomerUtils.isValid(orders)) {
+    //Build object of Customer domain
+    public static Customer buildCustomer(@Valid Person person, @Valid String customerId) {
+        if (CustomerUtils.isValid(person) || CustomerUtils.isValid(customerId)) {
             return new Customer.Builder()
                     .setIdentity(person)
                     .setCustomerId(customerId)
-                    .setOrders(orders)
                     .build();
         } else {
             return null;
